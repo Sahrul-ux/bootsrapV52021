9ca7ae4b9da689ac2ef21a3d1811239c13e369df
Sahrul-ux <sahrulrajab11@gmail.com>
2021-08-02 13:55:39 +0700

    menambahkan shadow pada navbar

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
 
 <body>
     <!-- Navbar -->
-    <nav class="navbar navbar-expand-lg navbar-light" style="background-color: aliceblue; shadow-sm">
+    <nav class="navbar navbar-expand-lg navbar-light shadow-sm" style="background-color: aliceblue; ">
         <div class="container">
             <a class="navbar-brand" href="#">Miaw House</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
