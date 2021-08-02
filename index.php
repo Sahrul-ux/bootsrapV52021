073f4196636b920f844d6f09053e61a83d47284e
Sahrul-ux <sahrulrajab11@gmail.com>
2021-08-02 13:53:11 +0700

    menambahkam shadow small pada navbar

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
 
 <body>
     <!-- Navbar -->
-    <nav class="navbar navbar-expand-lg navbar-light" style="background-color: aliceblue;">
+    <nav class="navbar navbar-expand-lg navbar-light" style="background-color: aliceblue; shadow-sm">
         <div class="container">
             <a class="navbar-brand" href="#">Miaw House</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
